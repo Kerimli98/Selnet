ff90ca513899f353c138082a654dd480f5c306a5
Emin <emin.134.134@gmail.com>
2021-05-18 23:03:01 +0400

    notes on js and google maps integration

--- javascript/script.js
+++ javascript/script.js
@@ -4,34 +4,35 @@ AOS.init({
 
 var a = 0;
 $(window).scroll(function () {
-
-    var oTop = $('#counter').offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-        $('.counter-value').each(function () {
-            var $this = $(this),
-                countTo = $this.attr('data-count');
-            $({
-                countNum: $this.text()
-            }).animate({
-                    countNum: countTo
-                },
-
-                {
-                    duration: 3000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.floor(this.countNum));
+    // umummiyyetle eger hansisa bir elementden asili bir code hissesi varsa birinci o elementin sehifede olub olmadigini yoxla
+    if ($('#counter').length) {
+        var oTop = $('#counter').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.counter-value').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                        countNum: countTo
                     },
-                    complete: function () {
-                        $this.text(this.countNum);
-                        //alert('finished');
-                    }
 
-                });
-        });
-        a = 1;
+                    {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                            //alert('finished');
+                        }
+
+                    });
+            });
+            a = 1;
+        }
     }
-
 });
 
 
